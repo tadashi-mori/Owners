11e11cd25769c9f4a8e34127eba844e30edc25dd
tadashi-mori <tadashi.mori.1110@gmail.com>
2017-09-04 17:37:33 +0900

    add user registration function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'projects#index'
+  post "users/create" => "users#create"
   get "signup" => "users#new"
   resources :projects
   resources :users
