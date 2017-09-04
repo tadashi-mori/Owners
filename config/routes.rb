604c8b8578ad1b15932cf55f17a2abeab6e2cdae
tadashi-mori <tadashi.mori.1110@gmail.com>
2017-09-04 15:57:12 +0900

    add signup route and users/new view haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'projects#index'
+  get "signup" => "users#new"
   resources :projects
   resources :users
   resources :articles
