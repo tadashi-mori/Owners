0dd6673669441c094f300979d3518c0d2e18e9b0
tadashi-mori <tadashi.mori.1110@gmail.com>
2017-09-18 16:42:07 +0900

    add modal login

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -2,3 +2,11 @@
 $(document).ready(function(){
   $(".player").YTPlayer();
 });
+
+$(function() {
+  // 「#login-show」要素に対するclickイベントを作成してください
+  $('#login-show').click(function(){
+    $('#login-modal').fadeIn();
+  });
+
+});
