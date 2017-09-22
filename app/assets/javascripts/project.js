a37d936690d05f8058ebeb3b3379e01c91351026
tadashi-mori <tadashi.mori.1110@gmail.com>
2017-09-22 14:13:01 +0900

    add scroll fanction

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -4,20 +4,30 @@ $(document).ready(function(){
 });
 
 // modal-login部分
-$(function() {
-  $('#login-show').click(function(){
+  $(function() {
+    $('#login-show').click(function(){
     $('#login-modal').fadeIn();
   });
 
 // modal-signup部分
-$('.signup-show').click(function(){
+  $('.signup-show').click(function(){
     $('#signup-modal').fadeIn();
   });
 
 // modal取り消し部分
- $('.close-modal').click(function(){
+  $('.close-modal').click(function(){
     $('#login-modal').fadeOut();
     $('#signup-modal').fadeOut();
+  });
+
+// ナビゲーション部分
+  $('.about-btn').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html, body').animate({
+      'scrollTop': position
+    }, 500);
   });
 
 });
